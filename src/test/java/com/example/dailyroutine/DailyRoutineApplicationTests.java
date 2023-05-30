67e279e611f63577c556ed0f86b3e5150826d608
kongji <kongji4092@gmail.com>
2023-05-30 14:27:24 +0900

    initial commit

---
+++ src/test/java/com/example/dailyroutine/DailyRoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dailyroutine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyRoutineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
